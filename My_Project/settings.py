5c92b4c88a600c3221580ae69967e834434ca2a0
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-25 14:56:57 +0000

    Test

--- My_Project/settings.py
+++ My_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-81l1@r)t9n11n7&w6ps02t)^-jv_nq6$xv1^!#&c4$*^9_wwm8
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'fasoalerteprobleme.herokuapp.com', 
+    'fasoalerteproblème.herokuapp.com', 
     '127.0.0.1',
     'localhost'
 ]
