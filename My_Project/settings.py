0bae3b664521b588af8e5d14e68e6d7af9c74ab5
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-25 19:09:50 +0000

    test

--- My_Project/settings.py
+++ My_Project/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'fasoalerteprobleme.herokuapp.com',
-    'alertefasoprobleme.herokuapp.com',
+    # 'alertefasoprobleme.herokuapp.com',
     '127.0.0.1',
     'localhost',
     '102.180.21.251'
