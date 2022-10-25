de3063214c458944943cd471c61c867ba25c86ac
AbdoulSAWADOGO <abdoulkadersawadogo7625@gmail.com>
2022-10-25 16:24:23 +0000

    test

--- My_Project/settings.py
+++ My_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'fasoalerteproblème.herokuapp.com', 
     '127.0.0.1',
-    'localhost'
+    'localhost',
+    '102.180.21.251'
 ]
 
 
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
