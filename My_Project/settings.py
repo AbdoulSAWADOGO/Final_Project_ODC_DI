d8bcd3c203fff90f913e301906d576680e795c86
Abdoul Kader SAWADOGO <110843605+AbdoulSAWADOGO@users.noreply.github.com>
2022-11-21 12:27:19 +0000

    commit

--- My_Project/settings.py
+++ My_Project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-81l1@r)t9n11n7&w6ps02t)^-jv_nq6$xv1^!#&c4$*^9_wwm8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     # 'fasoalerteprobleme.herokuapp.com',
